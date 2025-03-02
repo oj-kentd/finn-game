bfacdb9c6e603265aadc9d72ebdc4c10cd37a2a0
Danny <danny@openjaus.com>
2025-03-02 11:38:28 -0500

    v0.8.25

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ class Game {
     constructor() {
         // ----
         // (DO NOT DELETE THIS LINE)
-        this.version = "v0.8.24"; // Increment version UP
+        this.version = "v0.8.25"; // Increment version UP
         this.version += ` (${new Date().toISOString().slice(0, 19)})`;
         // ----
 
